684a7d32341102bac4ffb3a2a26d31a89eeb13f0
adriano25klein@gmail.com <you@example.com>
2022-01-10 09:53:07 -0300

    Tratando as exceções

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+
 import { routes } from "./routes";
 
 const app = express();
@@ -11,5 +13,17 @@ app.get("/", (req, res) => {
     message: "Hello World",
   });
 });
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({ message: err.message });
+    }
+
+    return response
+      .status(500)
+      .json({ status: "error", message: "Internal server error" });
+  }
+);
 
 app.listen(3000, () => console.log("Server is running"));
