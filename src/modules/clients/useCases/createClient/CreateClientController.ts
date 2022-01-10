81886c38c76737376f5057005999d1d01a245ad7
adriano25klein@gmail.com <you@example.com>
2022-01-10 08:57:38 -0300

    Cadastro de cliente

---
+++ src/modules/clients/useCases/createClient/CreateClientController.ts
@@ -0,0 +1,14 @@
+import { CreateClientUseCase } from "./CreateClientUseCase";
+import { Request, Response } from "express";
+
+export class CreateClientController {
+  async handle(request: Request, response: Response) {
+    const { username, password } = request.body;
+    const createClientUseCase = new CreateClientUseCase();
+    const result = await createClientUseCase.execute({
+      username,
+      password,
+    });
+    response.json(result);
+  }
+}
---
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -0,0 +1,36 @@
+import { prisma } from "../../../../database/prismaClient";
+import { hash } from "bcrypt";
+
+interface ICreateClient {
+  username: string;
+  password: string;
+}
+
+export class CreateClientUseCase {
+  async execute({ password, username }: ICreateClient) {
+    //TODO: validar se o cliente existe no banco
+    const clientExists = await prisma.clients.findFirst({
+      where: {
+        username: {
+          mode: "insensitive",
+        },
+      },
+    });
+    if (clientExists) {
+      throw new Error("Client already exists");
+    }
+
+    //TODO:Criptografar a senha do cliente
+    const hashPassword = await hash(password, 10);
+
+    //TODO:Salvar o novo cliente
+    const client = await prisma.clients.create({
+      data: {
+        username,
+        password: hashPassword,
+      },
+    });
+
+    return client;
+  }
+}
